21a6124ed2f306440da069d53e7de56fe6d22eaf
RameshNeupane <neupaneramesh555@gmail.com>
2023-10-15 15:36:00 +0545

    update repo title to the link

--- src/components/Repo/index.tsx
+++ src/components/Repo/index.tsx
@@ -4,36 +4,45 @@ import "./Repo.css";
 import { RepoProps } from "../../types/repos";
 
 const Repo = ({ repo }: RepoProps) => {
-  const { name, stargazers_count, html_url, description, language, homepage } =
-    repo;
+    const {
+        name,
+        stargazers_count,
+        html_url,
+        description,
+        language,
+        homepage,
+    } = repo;
 
-  return (
-    <div className="repo">
-      <div className="repo-top">
-        <div className="repo-name">{name}</div>
-        <div className="repo-star">
-          <span>
-            <FaStar />
-          </span>
-          <span>{stargazers_count}</span>
+    return (
+        <div className="repo">
+            <div className="repo-top">
+                <a
+                    href={html_url}
+                    target="_blank"
+                    rel="noreferrer"
+                    className="repo-name"
+                    title="Github"
+                >
+                    {name}
+                </a>
+                <div className="repo-star">
+                    <span>
+                        <FaStar />
+                    </span>
+                    <span>{stargazers_count}</span>
+                </div>
+                {homepage && (
+                    <div className="homepage-link">
+                        <a href={homepage} target="_blank" rel="noreferrer">
+                            view demo
+                        </a>
+                    </div>
+                )}
+            </div>
+            <p className="description">{description}</p>
+            <div className="language">made with: {language}</div>
         </div>
-        <div className="repo-link">
-          <a href={html_url} target="_blank" rel="noreferrer">
-            view repo
-          </a>
-        </div>
-        {homepage && (
-          <div className="homepage-link">
-            <a href={homepage} target="_blank" rel="noreferrer">
-              view demo
-            </a>
-          </div>
-        )}
-      </div>
-      <p className="description">{description}</p>
-      <div className="language">made with: {language}</div>
-    </div>
-  );
+    );
 };
 
 export default Repo;
